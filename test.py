976f7c0f28faccc20bade59c740972ec40cc3f34
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2021-01-16 15:08:55 -0500

    test midi script

---
+++ test.py
@@ -0,0 +1,21 @@
+import time
+from amidiw import MIDIInterface
+
+# set midi cc code
+CC_EXPRESSION = 11
+
+# create midi interface
+midi = MIDIInterface()
+
+# wipe right
+for i in range(128):
+    print(i)
+    midi.send_cc_message(CC_EXPRESSION, 1, i)
+    time.sleep(0.001)
+
+# wipe back left
+for i in range(128):
+    let ii = 127 - i
+    print(ii)
+    midi.send_cc_message(CC_EXPRESSION, 1, ii)
+    time.sleep(0.001)
