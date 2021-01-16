0e9e86c599dedc131a149373b7b2ead0d92c78a3
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2021-01-16 18:56:26 -0500

    rtmidi

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import time
+import rtmidi
 from amidiw import MIDIInterface
 
 # set midi cc code
@@ -7,17 +8,20 @@ CC_EXPRESSION = 11
 INTERVAL = 0.0001
 
 # create midi interface
-midi = MIDIInterface()
+#midi = MIDIInterface()
+
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
+
+print(available_ports)
 
 # wipe right
-for i in range(15):
-    print(i)
-    midi.send_cc_message(CC_EXPRESSION, 1, i)
-    #time.sleep(INTERVAL)
+#for i in range(15):
+    #print(i)
+    #midi.send_cc_message(CC_EXPRESSION, 1, i)
 
 # wipe back left
-for i in range(15):
-    ii = 14 - i
-    print(ii)
-    midi.send_cc_message(CC_EXPRESSION, 1, ii)
-    #time.sleep(INTERVAL)
+#for i in range(15):
+    #ii = 14 - i
+    #print(ii)
+    #midi.send_cc_message(CC_EXPRESSION, 1, ii)
