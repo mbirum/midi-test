36cb2b0af4012b173b5993be05d6957c92a46636
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2021-01-17 10:23:56 -0500

    rtpmidi

---
+++ rtmidiw.py
@@ -0,0 +1,32 @@
+import subprocess
+import rtmidi
+
+class MIDIInterface:
+    midiout = None
+    port_index = -1
+    channel_hex_base = 177
+    channel_base = 0
+
+    def __init__(self):
+        self.check_device()
+
+    def check_device(self):
+        self.midiout = rtmidi.MidiOut()
+        available_ports = self.midiout.get_ports()
+
+        for i in range(len(available_ports)):
+            if "rtpmidi" in available_ports[i]:
+                try:
+                    self.port_index = i
+                    self.midiout.open_port(i)
+                    print("rtpmidi port registered")
+                except:
+                    raise Exception('rtpmidi port could not be opened')
+                
+        if self.port_index == -1:
+            raise Exception('rtpmidi port not available')
+
+    def send_cc_message(self, cc, channel, value):
+        if self.port_index != -1:
+            channel_hex = hex(self.channel_hex_base + self.channel_base + channel)
+            self.midiout.send_message([channel_hex, cc, value])
--- test.py
+++ test.py
@@ -1,33 +1,20 @@
 import time
-import rtmidi
+from rtmidiw import MIDIInterface
 
-CHANNEL = 0xB0
 CC_EXPRESSION = 11
+CHANNEL = 0
 
 INTERVAL = 0.1
 
-midiout = rtmidi.MidiOut()
-available_ports = midiout.get_ports()
-port_index = -1
-
-
-for i in range(len(available_ports)):
-    if "rtpmidi" in available_ports[i]:
-        print(available_ports[i])
-        port_index = i
-
-if port_index == -1:
-    raise Exception('rtpmidi port not available')
-
-midiout.open_port(port_index)
+midi = MIDIInterface()
 
 for i in range(60):
     print(i)
-    midiout.send_message([CHANNEL, CC_EXPRESSION, i])
+    midi.send_cc_message(CC_EXPRESSION, CHANNEL, i)
     time.sleep(INTERVAL)
 
 for i in range(60):
     ii = 59 - i
     print(ii)
-    midiout.send_message([CHANNEL, CC_EXPRESSION, ii])
+    midi.send_cc_message(CC_EXPRESSION, CHANNEL, ii)
     time.sleep(INTERVAL)
